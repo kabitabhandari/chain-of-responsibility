3e4efa32b2a228a743ae9b9850fb68faa610d69e
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2022-02-02 14:56:10 -0600

    first commit

--- src/main/java/com/chain/of/responsibility/starttochain/StartToChainApplication.java
+++ src/main/java/com/chain/of/responsibility/starttochain/StartToChainApplication.java
@@ -1,18 +1,17 @@
 package com.chain.of.responsibility.starttochain;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StartToChainApplication {
 
-	public static void main(String[] args) {
-		AbstractLogger loggerChain = AbstractLogger.getChainOfLoggers();
-		loggerChain.logMessage(AbstractLogger.INFO, "This is an information");
-		System.out.println();
-		loggerChain.logMessage(AbstractLogger.DEBUG, "This is debug level");
-		System.out.println();
-		loggerChain.logMessage(AbstractLogger.ERROR, "This is an error level");
-	}
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = AbstractLogger.getChainOfLoggers();
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information");
+        System.out.println();
+        loggerChain.logMessage(AbstractLogger.DEBUG, "This is debug level");
+        System.out.println();
+        loggerChain.logMessage(AbstractLogger.ERROR, "This is an error level");
+    }
 
 }
